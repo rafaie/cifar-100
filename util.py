8ca93369a60879b7794ab829ede87d24c8f1721f
Mostafa <mostafa.rafaie@gmail.com>
2019-02-27 00:15:23 -0600

    Add a util function!

--- util.py
+++ util.py
@@ -116,6 +116,22 @@ def get_dataset2():
 
      return (ds_train, ds_test)
 
+def load_data3(data_dir='./data', test_size = 0.10):
+    # Load train data 
+    images = np.load(os.path.join(data_dir, 'cifar_images.npy'))
+    images = images.reshape((len(images), 32, 32, 3)).astype(np.float32)
+    labels_tmp = np.load(os.path.join(data_dir, 'cifar_labels.npy'))
+    labels = labels_tmp.astype(np.int32)
+
+    s1 = round(images.shape[0] * (1 - test_size))
+    train_x = images[:s1]
+    train_y = labels[:s1]
+    test_x = images[s1:]
+    test_y = labels[s1:]
+
+    print(train_x.shape, test_x.shape)
+    return (train_x, train_y, test_x, test_y, images, labels)
+
 
 # Operation Util class copied from this project: https://github.com/vahidk/TensorflowFramework
 
