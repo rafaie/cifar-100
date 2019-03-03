49f4912c0307837c84b56e43efa7c48cce473900
Mostafa <mostafa.rafaie@gmail.com>
2019-03-03 01:56:06 -0600

    Major update on autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -5,6 +5,7 @@ autoencoder.py: An example of autoencoder trained by ImageNet dataset
 """
 
 from util import get_dataset, get_dataset2
+from model import BaseConfig, BaseModel
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -13,6 +14,7 @@ from functools import reduce
 import argparse
 import os
 import util
+import sys
 
 
 def load_imagenet_dataset(data_dir='data'):
@@ -160,14 +162,14 @@ def vae_loss(inputs, outputs, latent_mean, latent_log_scale, output_dist, output
     
     return reconstruction_loss, latent_loss
 
-def get_autoencoder_vae_logistic(img_shape=[32, 32, 3], latent_size=100):
+def get_autoencoder_vae_logistic(img_shape=[32, 32, 3], latent_size=3):
     tf.reset_default_graph()
-    inputs = tf.placeholder(tf.float32, shape=[None] + img_shape)
+    inputs = tf.placeholder(tf.float32, shape=[None] + img_shape, name='input_placeholder')
     # VAE
-    with tf.variable_scope("model") as scope:
-        means, log_scales = gaussian_encoder(inputs, latent_size)
-        codes = gaussian_sample(means, log_scales)
-        outputs = decoder(codes, img_shape)
+    means, log_scales = gaussian_encoder(inputs, latent_size)
+    codes = gaussian_sample(means, log_scales)
+    codes = tf.identity(codes, name='codes')
+    outputs = decoder(codes, img_shape)
     # with tf.variable_scope("model", reuse=True) as scope:
     #     gen_sample = decoder(codes, img_shape)
 
@@ -181,14 +183,15 @@ def get_autoencoder_vae_logistic(img_shape=[32, 32, 3], latent_size=100):
     return inputs, codes, outputs, total_loss, output_log_scale
 
 
-def get_autoencoder_sparse(shape=[32, 32, 3], sparsity_weight=5e-3, code_size=100):
+def get_autoencoder_sparse(shape=[32, 32, 3], sparsity_weight=5e-3, code_size=192):
     tf.reset_default_graph()
-    x = tf.placeholder(tf.float32, [None] + shape, name='input')
+    x = tf.placeholder(tf.float32, [None] + shape, name='input_placeholder')
     encoder_16 = downscale_block(x)
     encoder_8 = downscale_block(encoder_16)
     flatten_dim = np.prod(encoder_8.get_shape().as_list()[1:])
     flat = tf.reshape(encoder_8, [-1, flatten_dim])
-    code = tf.layers.dense(flat, code_size, activation=tf.nn.relu, name='code')
+    code = tf.layers.dense(flat, code_size, activation=tf.nn.relu, name='codes_id')
+    code = tf.identity(code, name='codes')
     hidden_decoder = tf.layers.dense(code, 64, activation=tf.nn.elu)
     decoder_8 = tf.reshape(hidden_decoder, [-1, 8, 8, 1])
     decoder_16 = upscale_block(decoder_8)
@@ -205,15 +208,16 @@ def get_autoencoder_sparse(shape=[32, 32, 3], sparsity_weight=5e-3, code_size=10
     return x, code, output, total_loss, output_log_scale
 
 
-def get_autoencoder_auto_noise(shape=[32, 32, 3], sparsity_weight=5e-3, code_size=100, noise_level = 0.1):
+def get_autoencoder_auto_noise(shape=[32, 32, 3], sparsity_weight=5e-3, code_size=192, noise_level = 0.1):
     tf.reset_default_graph()
-    x = tf.placeholder(tf.float32, [None] + shape, name='input')
+    x = tf.placeholder(tf.float32, [None] + shape, name='input_placeholder')
     x_noisy = x + noise_level * tf.random_normal(tf.shape(x))
     encoder_16 = downscale_block(x_noisy)
     encoder_8 = downscale_block(encoder_16)
     flatten_dim = np.prod(encoder_8.get_shape().as_list()[1:])
     flat = tf.reshape(encoder_8, [-1, flatten_dim])
-    code = tf.layers.dense(flat, code_size, activation=tf.nn.relu, name='code')
+    code = tf.layers.dense(flat, code_size, activation=tf.nn.relu, name='codes_id')
+    code = tf.identity(code, name='codes')
     hidden_decoder = tf.layers.dense(code, 64, activation=tf.nn.elu)
     decoder_8 = tf.reshape(hidden_decoder, [-1, 8, 8, 1])
     decoder_16 = upscale_block(decoder_8)
@@ -279,9 +283,9 @@ def train_autoencoders():
         batch_size = 16
         session = tf.Session()
         session.run(tf.global_variables_initializer())
-        for epoch in range(1):
+        for epoch in range(100):
             print("Start epoch :", epoch)
-            for i in range(train_data.shape[0] // batch_size)[:100]:
+            for i in range(train_data.shape[0] // batch_size):
                 batch_xs = train_data[i*batch_size:(i+1)*batch_size, :]
                 session.run(train_op, {inputs: batch_xs})
         print("Training of the " + k + " autoencoder is done!")
@@ -296,9 +300,118 @@ def train_autoencoders():
         # save model
         save_model(session, k)
 
+def load_autoencoder(autoenconder_name ):
+    session = tf.Session()
+    saver = tf.train.import_meta_graph(os.path.join('auto_models', autoenconder_name + '.meta'))
+    saver.restore(session, os.path.join('auto_models', autoenconder_name))
+    graph = session.graph
+    input_placeholder = graph.get_tensor_by_name('input_placeholder:0')
+    code = graph.get_tensor_by_name('codes:0')
+    print(code.shape)
+    if autoenconder_name is not 'autoencoder_vae_logistic':
+        code = tf.reshape(code, [-1, 8, 8, 3])
+    return input_placeholder, code
+
+
+def get_model(autoenconder_name):
+    
+    input_placeholder, code = load_autoencoder(autoenconder_name)
+
+    features = util.conv_layers(code,
+                                   filters=[64, 192, 384, 256, 256],
+                                   kernels=[3, 3, 3, 3, 3],
+                                   pool_sizes=[2, 2, 2, 2, 2])
+    features = tf.contrib.layers.flatten(features)
+
+    logits = util.dense_layers(
+                features, [512, 100],
+                drop_rates=0.5,
+                linear_top_layer=True)
+
+    logits = tf.identity(logits, name='output')
+
+    labels = tf.placeholder(tf.int64, [None], name='label')
+
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    total_loss = loss #+ 0.02 * sum(reg_losses)
+
+    return logits, total_loss, input_placeholder, labels
+
+def evaluate(test_images, test_labels, total_loss, output, session,
+                 x, y, batch_size, test_num_examples):
+    ce_vals = []
+    conf_mxs = []
+
+    c_pred = tf.cast(tf.equal(y, 
+                                tf.argmax(output, axis=1)), tf.int32)
+    c_preds = []
+    for i in range(test_num_examples // batch_size):
+        batch_xs = test_images[i * batch_size:(i + 1) * batch_size]
+        batch_ys = test_labels[i * batch_size:(i + 1) * batch_size]
+        test_ce,  c_pred_val= session.run(
+            [tf.reduce_mean(total_loss), c_pred], {
+                x: batch_xs,
+                y: batch_ys
+            })
+        ce_vals.append(test_ce)
+        c_preds += c_pred_val.tolist()
+    return (ce_vals, conf_mxs, sum(c_preds)/len(c_preds))
+
+def train_and_evaluate(autoenconder_name, train_images, train_labels, test_images, test_labels):
+    train_num_examples = train_images.shape[0]
+    test_num_examples = test_images.shape[0]
+
+    output, total_loss, x, y = get_model(autoenconder_name)
+    
+ 
+    with tf.Session() as session:
+        with tf.variable_scope("model_1") as scope:
+            global_step_tensor = tf.get_variable(
+                    'global_step', trainable=False, shape=[], initializer=tf.zeros_initializer)
+            optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+            train_op = optimizer.minimize(total_loss, global_step=global_step_tensor)
+            session.run(tf.global_variables_initializer())
+            batch_size = 120
+            ce_vals = []
+            print(train_num_examples // batch_size)
+            for j in range(500):
+                for i in range(train_num_examples // batch_size):
+                    batch_xs = train_images[i * batch_size:(i + 1) * batch_size]
+                    batch_ys = train_labels[i * batch_size:(i + 1) * batch_size]
+                    _, train_ce = session.run(
+                        [train_op, tf.reduce_mean(total_loss)], {
+                            x: batch_xs,
+                            y: batch_ys
+                        })
+                    ce_vals.append(train_ce)
+                    avg_train_ce = sum(ce_vals) / len(ce_vals)
+
+                    if i % 50 == 0 and i > 10:
+                        print('epoch:', j, ',step:', i, ', TRAIN CROSS ENTROPY: ' + str(avg_train_ce))
+                        ce_vals, conf_mxs, acc = evaluate(test_images, test_labels, total_loss, output, session,
+                                                          x, y, batch_size, test_num_examples)
+                        avg_test_ce = sum(ce_vals) / len(ce_vals)
+                        print('------------------------------')
+                        print('TEST CROSS ENTROPY: ' + str(avg_test_ce))
+                        print('TEST Accuracy     : ' + str(acc))
+                        print(','.join(['TEST_PROGRESS', autoenconder_name, str(j), str(i), str(avg_train_ce), 
+                                            str(avg_test_ce), str(acc)]))
+
+
+
+
 
 def reuse_encoders():
-    pass
+    counter = 0
+    train_x, train_y, test_x, test_y = util.prepare_data()
+
+    for k, f in encoder_funcs.items():
+        print('Start reusing the ' + k + ' autoencoder!')
+        # train the model
+        train_and_evaluate(k, train_x, train_y, test_x, test_y)
+        sys.exit(0)
+
 
 
 if __name__ == "__main__":
@@ -312,6 +425,9 @@ if __name__ == "__main__":
         train_autoencoders()
     elif args.action == 'reuse':
         reuse_encoders()
+    elif args.action == 'all':
+        train_autoencoders()
+        reuse_encoders()
     else:
-        print('Please use with this param: -action [train|reuse]')
+        print('Please use with this param: -action [train|reuse|all]')
  
